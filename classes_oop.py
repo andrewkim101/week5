f727c4af1a006aaa631887e8c1147ea63b844259
ahusanov <38414895+ahusanov85@users.noreply.github.com>
2019-08-15 21:24:19 -0400

    week5 oop concepts loaded

---
+++ classes_oop.py
@@ -0,0 +1,68 @@
+# OOP features
+
+class Car(): # in class name follow CamelCase format
+    """general class for a model of a car"""
+    __maxspeed = 150 
+
+    def __init__(self, year, make, model):
+        self.year = year
+        self.make = make
+        self.model = model
+        self.odometer_reading = 0
+    
+    def get_descriptive_name(self, color = 'white'):
+        """returns the full desc of the car"""
+        print(f"Car description: {color} {self.year} {self.make} {self.model}")
+
+    def get_odometer_reading(self):
+        """returns total milage of the car"""
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def set_odometer(self, milage):
+        """sets the odometer of the car, raject if milage is less than current milage"""
+        if milage >= self.odometer_reading:
+            self.odometer_reading = milage
+        else:
+            print("You can not roll back odometer.")
+
+    def increment_odometer(self, miles):
+        # self.odometer_reading = self.odometer_reading + miles
+        if miles >=0 :
+            self.odometer_reading += miles
+        else: 
+            print("You can not add negative miles to odomter")
+
+    def get_maxspeed(self):
+        print(f"Maximum speed in this car is : {self.__maxspeed}.")
+
+    def set_maxspeed(self, speed):
+        self.__maxspeed = speed
+
+    def fill_gas_tank(self):
+        print("Filling your gas tank ...")
+
+
+# INHERITANCE - inherits the contructor and the behaviour of the parent class 
+class ElectricCar(Car):
+    """Represents the aspects of a car, specific to an electric car"""
+
+    def __init__(self, year, make, model):
+        """Initialize attributes of the parent class and specific to electric car."""
+        super().__init__(year, make, model)
+        self.battery_size = 80
+
+    def get_battery_size(self):
+        """returns the size of battery"""
+        print(f"battery size : {self.battery_size}-kWh.")
+
+    def set_battery_size(self, battery_size):
+        if battery_size >= self.battery_size :
+            self.battery_size = battery_size
+        else:
+            print(f"You can not update the batter for less than {self.battery_size}-kwh")
+
+    # def udpate_maxspeed(self, speed):
+    #     self.__maxpeed = speed
+
+    def fill_gas_tank(self):
+        print("Electric cars do not have gas tank.")
---
+++ classes_oop_test.py
@@ -0,0 +1,63 @@
+# from classes_oop import * 
+from classes_oop import Car, ElectricCar
+
+import hello
+# greet()
+hello.greet()
+
+
+# ------- default constructor
+# camry = Car() 
+# camry.year = 2018
+# camry.make = 'toyota'
+# camry.model = 'camry'
+
+camry = Car(2018, 'toyota', 'camry')
+camry.get_descriptive_name()
+
+# ----- accessing the attributes directly 
+camry.odometer_reading = 1000
+camry.get_odometer_reading()
+# camry.odometer_reading = 900
+# camry.set_odometer(900)
+
+# accessing (updating) the attribute through methos, you can put some logic 
+camry.set_odometer(2000)
+camry.get_odometer_reading()
+
+camry.increment_odometer(200)
+camry.get_odometer_reading()
+
+camry.increment_odometer(-200)
+camry.get_odometer_reading()
+print("==============================")
+# ------
+# inheritance, encapsulation, polymorphism, method overriding 
+
+# polymorphism - using the same methods from different classes or using the same method with diff. attributes
+print(len('hello'))
+print(len([4, 5, 6, 712, 3453456, 23 ]))
+camry.get_descriptive_name()
+camry.get_descriptive_name('black')
+
+# encapsulation - concept of hiding or limiting the access to certain methods or variables in the class
+# create the instance variable in the class >> __maxspeed
+# camry.__maxspeed = 200  
+camry.set_maxspeed(200)
+camry.get_maxspeed()
+
+
+# ELECTRIC CAR - tests for child class of Car class
+my_tesla = ElectricCar(2019, 'tesla', 'model s')
+print(my_tesla.battery_size)
+my_tesla.get_descriptive_name()
+my_tesla.get_battery_size()
+my_tesla.set_battery_size(70)
+my_tesla.get_battery_size()
+my_tesla.set_battery_size(100)
+my_tesla.get_battery_size()
+
+my_tesla.set_maxspeed(210)
+
+camry.fill_gas_tank()
+my_tesla.fill_gas_tank()
